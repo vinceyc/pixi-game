e8df053ed56da011e102a74808a21f20e62f372a
Vincent Chan <vince.ys.chan@gmail.com>
2015-07-29 17:34:45 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+
+    <link href='http://fonts.googleapis.com/css?family=Karla:700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="style.css" css="">
+
+  </head>
+  <body>
+
+    <div id="stage">
+
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/phaser/2.4.1/phaser.min.js"></script>
+    <script src="App.js"></script>
+
+  </body>
+</html>
